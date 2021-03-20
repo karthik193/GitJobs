a0eb1371eecb8496c134cfe3dcd42bba18a7eca4
Karthik Pasupulatei <karthik.pasupulatei@gmail.com>
2021-03-20 12:51:45 +0530

    done

--- git_jobs/src/App.js
+++ git_jobs/src/App.js
@@ -6,7 +6,6 @@ import './App.css';
 const Light  = 'white' ; 
 const Dark = 'black' ; 
 function GetIcon(props){
-  console.log(props); 
   if(props.type === 'dark') return (<span>🌜</span>) ; 
   else return(<span>🌞</span>) ;
 }
@@ -16,9 +15,14 @@ function App() {
   const [ResultList , setRL] = useState([])  ;  
   const [SearchLog , setSL] =useState({
     description : '' , 
-    Location :'', 
+    location :'', 
     Full_Time : false
   }) ; 
+  const [jobDetails , setJD] = useState({
+    flag : false , 
+    details : 'none' 
+  });
+  if(ResultList.length == 0 )GetRequest('Ruby' , '' , false,setRL) ;
   function manageMode(){
     document.body.style.backgroundColor = Switch? Light : Dark ; 
     document.body.style.color = !Switch? Light : Dark ; 
@@ -26,7 +30,11 @@ function App() {
   }
   
   function manageChange(event){
-    var {name ,value} = event.target  ;  
+    var {name ,value} = event.target  ; 
+    if (name == 'Full_Time'){
+      if (value == 'on' )value = true; 
+      else value = false; 
+    } 
     setSL(preVal =>{
       return ({
         ...preVal , 
@@ -38,8 +46,13 @@ function App() {
   
   function manageSearch(event){
     event.preventDefault();
-    console.log(event) ; 
-    GetRequest(SearchLog.description , SearchLog.Location , SearchLog.Full_Tim , setRL) ;  
+    console.log(SearchLog) ; 
+    setJD(preVal =>({
+      ...preVal ,
+      flag : false
+    })) ; 
+    if(SearchLog.description == ''&&SearchLog.Location =='' &&SearchLog.Full_Tim == false )setRL([]) ;  
+    else GetRequest(SearchLog.description , SearchLog.Location , SearchLog.Full_Time , setRL) ;  
      
   }
   return (
@@ -55,25 +68,55 @@ function App() {
           <form onSubmit={manageSearch}>
             <div><input type = "text" className ='SearchBar' name = 'description'  onChange = {manageChange}></input></div>
             <div><input type = "text" className ='SearchBar' name = 'location' placeholder= 'Location' onChange = {manageChange}></input></div>
-            <div style = {{padding : '10px'}}><p>Full Time</p><input type = "checkBox" onChange = {manageChange}/></div>  
+            <div style = {{padding : '10px'}}><p>Full Time</p><input name = "Full_Time" type = "checkBox" onChange = {manageChange}/></div>  
             <button className ='handy' type = 'submit' style = {{margin:"10px"}} >Search</button>        
           </form>
         </div>
         
         </div>
+        {!jobDetails.flag?<div className="Results col2" >
+            <Tabs ResultList  = {ResultList}  setJD = {setJD}/>
+        </div>:
+        <div className="JobDetails col2" id = "jobDetails" style ={{padding :"10px"}}>
+            <div><img className = "logo"  src = {jobDetails.details.company_logo} style  = {{height : '100px'}}></img>
+            <h3>{jobDetails.details.company}</h3> 
+            <p>Title: {jobDetails.details.title}</p>
+            <p>type: {jobDetails.details.type}</p>
+            <p>location: {jobDetails.details.location}</p>
+           <p>visit our Page <a href = {jobDetails.details.company_url}>Here</a></p>
+            <p>description:</p>
+        <div dangerouslySetInnerHTML={{__html: jobDetails.details.description }} />
 
-        <div className="Results" id = "col2" >
-            <Tabs ResultList  = {ResultList}/>
+        <h2>HOW TO APPLY ?</h2>
+        <div dangerouslySetInnerHTML={{__html: jobDetails.details.how_to_apply }} />
         </div>
+        </div> }
+        
+        
       </div>
     </div>
   );
 }
+function DisplayJobDetails(jobDetails , setJD){
+  setJD({
+    flag : true , 
+    details : jobDetails
+  }) ; 
+
+}
 function Tabs(props){
+  if(props.ResultList.length == 0 )if( props.des ==''&& props.loc =='' && props.ft == false )return <h3>Loading results</h3> ; else return <h3>No Results found..<br/>Loading recommended jobs</h3> ; 
   return (<div style = {{display :'' , color :"black"}}>
     {props.ResultList.map((each , index)=>{
-      return (<div className = 'Tab' style = {{ boxShadow: "0 1px 5px rgb(138, 137, 137)" , float : 'left'}}>
-        <p>name: {each.company}</p>
+      
+      return (<div className = 'Tab handy' style = {{ boxShadow: "0 1px 5px rgb(138, 137, 137)" , float : 'left'}}
+        onClick = {()=>{DisplayJobDetails(each , props.setJD);}}
+      >
+        <img className = "logo"  src= {each.company_logo} ></img>
+        <h3>{each.company}</h3>
+        <p>title : {each.title}</p>
+        <p>locaiton : {each.location}</p>
+        <p>type : {each.type}</p>
       </div>)
     })}
   </div>)
--- git_jobs/src/GetInfo.js
+++ git_jobs/src/GetInfo.js
@@ -1,8 +1,13 @@
 import React from 'react'; 
 import axios from 'axios';
 
-async function GetRequest(description , Loc , FT , setRL){
-    return await axios.get('https://spring-feather-5dc4.karthik-pasupulatei.workers.dev/?https://jobs.github.com/positions.json?description=ruby&page=1').then(response=>{
+async function GetRequest(description , location , FT , setRL){
+    var url  = 'https://spring-feather-5dc4.karthik-pasupulatei.workers.dev/?https://jobs.github.com/positions.json?' ; 
+    if (description != '') url += '&description=' + description ; 
+    if(location != '') url += '&location=' + location ; 
+    if(FT != false) url += '&full_time=' + FT ;
+    url += '&page=1' ; 
+    return await axios.get(url).then(response=>{
         console.log('response',response.data)
         setRL(response.data); 
     }).catch(err=>console.log(err)) ; 
